efd7937fb3197a4b9948fa13055c33e36a08bf44
Charles Callender <charlescallender.cc@gmail.com>
2022-06-11 21:45:13 -0700

    Initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Home = () => {
+
+    return (
+        <Container>
+            <h1>Retroland</h1>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`
+
+export default Home;
