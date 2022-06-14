61a6a07141388486f6861899e0c3b20b4ecd40d0
Charles Callender <charlescallender.cc@gmail.com>
2022-06-13 22:08:24 -0700

    Updated sign

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 
     return (
         <Container>
-            <h1>Retroland</h1>
+            <Title>Retroland</Title>
 
             <SpaceInvaders />
             <Frogger />
@@ -19,7 +19,34 @@ const Home = () => {
 }
 
 const Container = styled.div`
+background-image: url('../arcade.jfif');
+background-size: 100px 100px;
+margin: 0;
 
+`
+
+const Title = styled.h1`
+    animation: animate 4s linear infinite;
+    font-size: 60px;
+    background-color: black;
+    text-transform: uppercase;
+    letter-spacing: 12px;
+    margin-top: 0;
+
+    @keyframes animate {
+        0% {
+            color: #8502c7;
+            text-shadow: none;
+        }
+         100% {
+            color: #ab02f2;
+            text-shadow: 0 0 10px #7100a1,
+            0 0 20px #7100a1,
+            0 0 40px #7100a1,
+            0 0 80px #7100a1,
+            0 0 160px #7100a1;
+        }
+    }
 `
 
 export default Home;
